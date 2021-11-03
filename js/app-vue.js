ef6b84f8696145e698f17b38601f7e14d9c32b69
Antonio Blanca <solucwebs@gmail.com>
2021-11-02 22:14:33 -0400

    inserta Mensaje aquí

--- js/app-vue.js
+++ js/app-vue.js
@@ -1,6 +1,32 @@
 var app = new Vue({
   el: '#header-navbar',
   data: {
-    logo: 'Hola Vue!'
+    logo: 'ANTOB DEV',
+    menu: [
+      {
+        nombre: 'Inicio',
+        link: '#inicio'
+      },
+      {
+        nombre: 'Servicios',
+        link: '#'
+      },
+      {
+        nombre: 'Portafolios',
+        link: '#'
+      },
+      {
+        nombre: 'Sobre mi',
+        link: '#'
+      },
+      {
+        nombre: 'Testimonios',
+        link: '#'
+      },
+      {
+        nombre: 'Contacto',
+        link: '#'
+      },
+    ]
   }
 })
