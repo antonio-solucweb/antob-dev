bcf7d891db8681101f7378f3147904a305a13a1e
Antonio Blanca <solucwebs@gmail.com>
2021-11-05 23:30:19 -0400

    Modified files and favicon

--- js/app-vue.js
+++ js/app-vue.js
@@ -213,21 +213,21 @@ var app3 = new Vue({
         mensaje: 'Gli sviluppatori web migliori e più creativi comprendono davvero bene le istruzioni e danno il loro 110% per consegnare il lavoro in tempo. Ho avuto a che fare con molti sviluppatori web e nessuno come il tuo team. Li consiglio vivamente !!!',
         proyecto: '69Livecam',
         pais: 'Italia',
-        imagen: 'photo-1.jpg'
+        imagen: 'testi-1.jpg'
       },
       {
         nombre: 'Maribel Martínez',
         mensaje: 'Excelente trabajo, webs súper modernas y funcionales que atraen clientes',
         proyecto: 'Tu Delivery En Casa,',
         pais: 'Venezuela',
-        imagen: 'photo-1.jpg'
+        imagen: 'testi-1.jpg'
       },
       {
         nombre: 'Macario Betancourt',
         mensaje: 'Diseño excelente, se ve muy bien desde móvil y se hace fácil navegar por ella',
         proyecto: 'Sticky Deal',
         pais: 'Venezuela',
-        imagen: 'photo-1.jpg'
+        imagen: 'testi-1.jpg'
       }
     ]
   },
