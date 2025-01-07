4623ce8858960857d8b242250d76102d2b54cafe
Antonio Blanca <solucwebs@gmail.com>
2025-01-07 08:04:39 -0300

    Added-laravel-to-skills

--- assets/translations.js
+++ assets/translations.js
@@ -2,12 +2,12 @@ window.translations = {
     "es": {
         //Bloque "Banner principal"
         "welcome_title": "Hola <span>,</span> soy Antonio Blanca.",
-        "welcome_description": "Soy ingeniero en computación con más de 6 años de experiencia en desarrollo web, apasionado por el CMS WordPress y sus increíbles características",
+        "welcome_description": "Soy ingeniero en informática con más de 6 años de experiencia en desarrollo web, apasionado por el CMS WordPress y sus increíbles características",
         "description": "Este es un sitio web de ejemplo para mostrar mi trabajo.",
         
         //Bloque "About Me"
         "about_title": "Sobre mí",
-        "about_welcome": "Soy un Ingeniero en Informática y desarrollador web apasionado por crear soluciones innovadoras y experiencias digitales únicas.",
+        "about_welcome": "Apasionado por crear soluciones innovadoras y experiencias digitales únicas.",
         "about_description": "Con más de 6 años de experiencia en desarrollo web, me he especializado en WordPress, el CMS que domina mi portafolio con más del 95% de los proyectos realizados. A lo largo de mi carrera, he llevado mis conocimientos en WordPress más allá de lo básico, dominando características avanzadas como la creación de temas y plugins personalizados, la gestión de sitios multisite y la integración de herramientas dinámicas como ACF y JetEngine. Mi compromiso es transformar ideas en soluciones digitales efectivas y funcionales.",
         "about_email": "Correo electrónico",
         
@@ -26,8 +26,9 @@ window.translations = {
         "skills_fe_responsive": "Diseño Responsivo",
         "skills_fe_bootstrap": "Framework Bootstrap",
         "skills_fe_sass": "SASS/SCSS",
+        "skills_fe_laravel": "LARAVEL",
 
-        "skills_tools_title": "Herramientas de Desarrollo",
+        "skills_tools_title": "Otras habilidades",
         "skills_tools_git": "Control de Versiones Git",
         "skills_tools_php": "PHP y MySQL",
         "skills_tools_api": "Integración REST API",
@@ -63,7 +64,7 @@ window.translations = {
 
         //Bloque "About Me"
         "about_title": "Sobre mim",
-        "about_welcome": "Sou Engenheiro de Informática e desenvolvedor web apaixonado por criar soluções inovadoras e experiências digitais únicas.",
+        "about_welcome": "Apaixonado por criar soluções inovadoras e experiências digitais únicas.",
         "about_description": "Com mais de 6 anos de experiência em desenvolvimento web, especializei-me no WordPress, o CMS que representa mais de 95% do meu portfólio. Ao longo da minha carreira, levei meu conhecimento sobre WordPress além do básico, dominando recursos avançados, como criação de temas e plugins personalizados, gestão de multisites e ferramentas dinâmicas como ACF e JetEngine. Meu compromisso é transformar ideias em soluções digitais eficazes e funcionais",
         "about_email": "E-mail",
 
@@ -82,8 +83,9 @@ window.translations = {
         "skills_fe_responsive": "Design Responsivo",
         "skills_fe_bootstrap": "Framework Bootstrap",
         "skills_fe_sass": "SASS/SCSS",
+        "skills_fe_laravel": "LARAVEL",
 
-        "skills_tools_title": "Ferramentas de Desenvolvimento",
+        "skills_tools_title": "Outras habilidades",
         "skills_tools_git": "Controle de Versão Git",
         "skills_tools_php": "PHP e MySQL",
         "skills_tools_api": "Integração REST API",
